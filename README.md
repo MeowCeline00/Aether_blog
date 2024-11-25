4210ac48b94fe774a0234a37d4aa92f943385c8c
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2024-11-24 17:55:58 -0800

    first commit

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-# Blogify - HTML & CSS Blog Template
->Blog Website Template built using HTML, CSS, some Bootstrap, and vanilla JavaScript.
-
-----
-
-## Visit the Live Site:
-
-https://nicholasroy96.github.io/blogify/
-
-----
-
-## Or Run Locally
-1. git clone https://github.com/NicholasRoy96/blogify.git
-2. open index.html file in browser
+# Aether Blog Page
--- js/index.js
+++ js/index.js
@@ -1,49 +1,11 @@
-function filterAll () {
+// Dark mode toggle
+const themeToggler = document.getElementById("theme-toggler");
 
-    // Resetting class of hidden on each card
-    let cards = [...document.getElementsByClassName("card")]
-    cards.forEach(card => {
-        card.classList.remove("hidden")
-    })
-
-    // Resetting class of active on each button
-    let categoryButtons = [...document.getElementsByClassName("category")]
-    categoryButtons.forEach(button => {
-        button.classList.remove("active")
-    })
-
-    // Adding class of active to button that has been pressed
-    let allButton = document.getElementById("all-button")
-    allButton.classList.add("active")
-
-    // Removing set background height and resetting to default height: 100%
-    let backgroundImage = [...document.getElementsByClassName("main-image")]
-    backgroundImage[0].classList.remove('main-image-filtered')
-    
-}
-
-function filterSelection(cardClass, buttonId) {
-
-    // Resetting class of hidden on each card then re-adding hidden to cards that have not been selected in filter
-    let cards = [...document.getElementsByClassName("card")]
-    cards.forEach(card => {
-        card.classList.remove("hidden")
-        if (!card.classList.contains(cardClass)) {
-            card.classList.add("hidden")
-        }
-    })
-
-    // Resetting class of active on each button
-    let categoryButtons = [...document.getElementsByClassName("category")]
-    categoryButtons.forEach(button => {
-        button.classList.remove("active")
-    })
-
-    // Adding class of active to button that has been pressed
-    let clickedButton = document.getElementById(buttonId)
-    clickedButton.classList.add("active")
-
-    // Adding set background height so background stretches entire viewport on all devices
-    let backgroundImage = [...document.getElementsByClassName("main-image")]
-    backgroundImage[0].classList.add('main-image-filtered')
-}
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    themeToggler.textContent = "☀️"; // Switch to sun icon
+  } else {
+    themeToggler.textContent = "🌙"; // Switch to moon icon
+  }
+});
